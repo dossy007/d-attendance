009daf6f918ab0b930638ef1c7e0a3d9b350b781
nishida-toshiharu <71758064+nishida-toshiharu@users.noreply.github.com>
2023-01-17 21:30:14 +0900

    firstcommit

--- site_project/site_project/settings.py
+++ site_project/site_project/settings.py
@@ -128,12 +128,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'Asia/Singapore'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
