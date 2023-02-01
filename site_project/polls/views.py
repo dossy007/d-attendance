6ba4323ded5e44a090d91c6d9fa4374b4f8985b9
nishida-toshiharu <71758064+nishida-toshiharu@users.noreply.github.com>
2023-02-01 22:43:33 +0900

    tutorial6 style.css 反映終了

--- site_project/polls/views.py
+++ site_project/polls/views.py
@@ -18,8 +18,9 @@ class IndexView(generic.ListView):
   
   def get_queryset(self):
     return Question.objects.filter(
-      pub_date_lte=timezone.now().orderby('-pub_date')[:5]
-    )
+      pub_date__lte=timezone.now()
+    ).order_by('-pub_date')[:5]
+    
   
 class DetailView(generic.ListView):
   model = Question
