2171198b31a15afc6af92176f7f3799481d98912
nishida-toshiharu <71758064+nishida-toshiharu@users.noreply.github.com>
2023-02-06 23:09:01 +0900

    tutorial7

--- site_project/polls/models.py
+++ site_project/polls/models.py
@@ -1,6 +1,7 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 # Create your models here.
 class Question(models.Model):
   question_text = models.CharField(max_length=200)
@@ -8,6 +9,13 @@ class Question(models.Model):
   
   def __str__(self):
     return self.question_text
+  
+  
+  @admin.display(
+    boolean=True,
+    ordering='pub_date',
+    description='published recently?',
+  )
   
   #classmethod
   def was_published_recently(self):
